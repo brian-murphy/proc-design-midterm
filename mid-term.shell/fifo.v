852807bce1dc5a306254b52ccbc3061e9aa09916
Brian Murphy <brianmurphy@gatech.edu>
2017-11-01 13:39:54 -0400

    a few more changes

--- mid-term.shell/fifo.v
+++ mid-term.shell/fifo.v
@@ -18,9 +18,34 @@ module fifo
 // ******************************************************************
 // your logic here ->
 // ******************************************************************
+
+reg [DATA_WIDTH - 1 : 0] data [ADDR_WIDTH - 1 : 0];
+
+reg should_sample_data_in;
+reg [ADDR_WIDTH - 1 : 0] in_index;
+
 wire next_in_index;
 
 counter #(ADDR_WIDTH) next_in_counter(clk, reset, enq, next_in_index);
+
+always @(posedge clk) begin 
+
+    if (reset == 1'b1) begin
+        should_sample_data_in <= 0;
+        in_index <= 0;
+        empty = 1'b1;
+
+    end else begin
+        if (should_sample_data_in == 1'b1) begin
+            data[in_index] <= data_in;
+            ///TODO
+        if (enq == 1'b1) begin
+            should_sample_data_in <= 1'b1;
+            in_index <= next_in_index;
+        end else
+            should_sample_data_in <= 1'b0;
+    end
+end
 
 // ******************************************************************
 
